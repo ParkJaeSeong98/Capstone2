b8dc27f939f2410cd3ecf7df26bc9805ad25671a
ohyangho <swyangho@gmail.com>
2024-06-06 13:08:26 +0900

    조건에 맞는 음식 없을 때 메세지

--- lib/main.dart
+++ lib/main.dart
@@ -177,7 +177,7 @@ class _HomeScreenState extends State<HomeScreen> {
   void _spinRoulette() async {
     if (_foodItems.isEmpty) return;
 
-    String selectedItem;
+    String? selectedItem;
     List<String> filteredFoodItems = [];
 
     if (_isHealthMode) {
@@ -204,11 +204,6 @@ class _HomeScreenState extends State<HomeScreen> {
         final random = Random();
         final randomIndex = random.nextInt(filteredFoodItems.length);
         selectedItem = filteredFoodItems[randomIndex];
-      } else {
-        // 조건에 맞는 음식이 없을 경우 전체 음식 중에서 랜덤 선택
-        final random = Random();
-        final randomIndex = random.nextInt(_foodItems.length);
-        selectedItem = _foodItems[randomIndex];
       }
     } else {
       // HealthMode OFF: 전체 음식 중에서 랜덤 선택
@@ -217,45 +212,80 @@ class _HomeScreenState extends State<HomeScreen> {
       selectedItem = _foodItems[randomIndex];
     }
 
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('오늘 점심은 이거다!'),
-          titlePadding: EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 30.0),
-          content: Text(
-            '$selectedItem',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
+    if (_isHealthMode && filteredFoodItems.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            titlePadding: EdgeInsets.all(16),
+            contentPadding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+            title: Text('알림'),
+            content: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    'assets/crying_cat.png',
+                    width: 400,
+                    height: 200,
+                  ),
+                  SizedBox(height: 16),
+                  Text('조건에 맞는 음식이 없습니다.'),
+                ],
+              ),
             ),
-            textAlign: TextAlign.center,
-          ),
-          contentPadding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0),
-          actions: [
-            Container(
-              margin: EdgeInsets.only(bottom: 10.0, right: 10.0),
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Color(0xFF57BD85),
-                  foregroundColor: Colors.white,
-                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                ),
+            actions: [
+              TextButton(
                 onPressed: () {
-                  Navigator.of(context).pop(); // First, close the dialog
-                  Navigator.push( // Then, navigate to the next page.
-                    context,
-                    MaterialPageRoute(builder: (context) => MenuRecommendationPage(mealTime: "lunch", menu: selectedItem)),
-                  );
+                  Navigator.of(context).pop();
                 },
                 child: Text('확인'),
               ),
+            ],
+          );
+        },
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('오늘 점심은 이거다!'),
+            titlePadding: EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 30.0),
+            content: Text(
+              '$selectedItem',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ],
-        );
-      },
-    );
+            contentPadding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0),
+            actions: [
+              Container(
+                margin: EdgeInsets.only(bottom: 10.0, right: 10.0),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xFF57BD85),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop(); // First, close the dialog
+                    Navigator.push( // Then, navigate to the next page.
+                      context,
+                      MaterialPageRoute(builder: (context) => MenuRecommendationPage(mealTime: "lunch", menu: selectedItem!)),
+                    );
+                  },
+                  child: Text('확인'),
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   @override
