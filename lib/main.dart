c2a288291f9f50daabae743d263b621d521288c9
ohyangho <swyangho@gmail.com>
2024-06-06 11:21:52 +0900

    메뉴 추천 메세지 롤백

--- lib/main.dart
+++ lib/main.dart
@@ -222,20 +222,35 @@ class _HomeScreenState extends State<HomeScreen> {
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text('오늘 점심은 이거다!'),
+          titlePadding: EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 30.0),
           content: Text(
             '$selectedItem',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center,
           ),
+          contentPadding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0),
           actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MenuRecommendationPage(mealTime: "lunch", menu: selectedItem)),
-                );
-              },
-              child: Text('확인'),
+            Container(
+              margin: EdgeInsets.only(bottom: 10.0, right: 10.0),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Color(0xFF57BD85),
+                  foregroundColor: Colors.white,
+                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop(); // First, close the dialog
+                  Navigator.push( // Then, navigate to the next page.
+                    context,
+                    MaterialPageRoute(builder: (context) => MenuRecommendationPage(mealTime: "lunch", menu: selectedItem)),
+                  );
+                },
+                child: Text('확인'),
+              ),
             ),
           ],
         );
